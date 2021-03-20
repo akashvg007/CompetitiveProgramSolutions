4e4ccc939eb4e73ed1fa9515cc27c4509656d958
Akash VG <akashvg007@gmail.com>
2021-03-20 16:08:04 +0530

    Longest Palindrome Substring

---
+++ longestPalindromeSubstring.js
@@ -0,0 +1,49 @@
+//longest Palindrome substring in a string
+
+const str = "abcba";
+const length = str.length;
+
+let MDArray = [];
+
+function printArr(arr) {
+  let result = "";
+  arr.map((item) => (result += item + "    "));
+  console.log(result + "\n");
+}
+
+for (let i = 0; i < length; i++) {
+  const subArr = new Array(length).fill(0);
+  subArr[i] = 1;
+  if (str[i + 1] && str[i] == str[i + 1]) subArr[i + 1] = 1;
+  MDArray.push(subArr);
+}
+
+let i = 0,
+  j = 2;
+let LPS = { start: 0, end: 0 };
+for (let p = 2; p < length; p++) {
+  j = p;
+  i = 0;
+  for (let k = 0; k < length; k++) {
+    if (str[i] == str[j] && MDArray[i + 1][j - 1] == 1) {
+      MDArray[i][j] = 1;
+      LPS.start = i;
+      LPS.end = j;
+    }
+    ++i;
+    ++j;
+    if (j > length) break;
+  }
+}
+
+MDArray.map((item) => printArr(item));
+
+const { start, end } = LPS;
+
+if (start == end) {
+  console.log("Not posible");
+  return;
+}
+
+console.log("longest palindrom indexs are", start, end);
+console.log("longest palindrom substring is", str.substring(start, end + 1));
